e1f8e80e24724c604a5aa9469d1de3d6322d1f65
Daniel Fitze <daniel.fitze@psy.unibe.ch>
2022-10-05 13:46:04 +0200

    added change of background color

--- img_convert.sh
+++ img_convert.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-lumin=74.12
+lumin=70
 
-#convert in/1.HEIC -colorspace Gray out/1.jpg
-
+# ========================================
+# change color
+# ========================================
+rm out/*
 cd in 
-for j in *.HEIC
+for j in *.jpg
 do 
-  magick "$j" -colorspace Gray -brightness-contrast "%[fx:${lumin}-(mean*100)]" ../out/"$j"
+  magick "$j" -colorspace Gray  ../out/"$j"
 done
+cd ../out
+
+# ========================================
+# change luminance
+# ========================================
+for j in *.jpg
+do 
+  magick "$j" -brightness-contrast "%[fx:${lumin}-(mean*100)]" "$j"
+done
+
+# ========================================
+# change background color
+# ========================================
+# colors:
+#   example grey:  #999999
+#   example white: #FFFFFF
+#   example red:   #FF0000
+for j in *.jpg
+do 
+  convert "$j" \
+    -fill '#FF0000' -fuzz 20% \
+    -draw 'color 0,0 floodfill' \
+    "$j"
+  done
+
+
 
